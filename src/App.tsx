53333489eee5247aa3e0cf757ef3cd4ee4603a0b
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-07-08 18:29:26 +0200

    Removed custom history and added route for jobpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,47 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
-import LandingPage from "./landingpage/LandingPage";
-import { setup } from "./static/util/axiosConfig";
-import { Router, Switch, Route } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import JobContextProvider from "./context/JobContextProvider";
-import NavBar from "./navigation/NavBar";
-import { CssBaseline } from "@material-ui/core";
+import LandingPage from "./components/landingpage/LandingPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import JobContextProvider from "./components/context/JobContextProvider";
+import NavBar from "./components/navigation/NavBar";
+import { CssBaseline, makeStyles, Theme, createStyles, MuiThemeProvider } from "@material-ui/core";
+import JobPageContainer from "./components/job/JobPageContainer";
 
-export const history = createBrowserHistory();
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            marginTop: theme.spacing(15),
+        },
+    })
+);
 
 function App() {
-    setup.setupInterceptors();
+    const classes = useStyles();
 
     return (
-        <Router history={history}>
-            <CssBaseline />
-            <JobContextProvider>
-                <NavBar />
-                <Container maxWidth="lg">
-                    <Switch>
-                        <Route exact component={LandingPage} path="/" />
-                    </Switch>
-                </Container>
-            </JobContextProvider>
-        </Router>
+        <MuiThemeProvider theme={theme}>
+            <Router>
+                <CssBaseline />
+                <JobContextProvider>
+                    <NavBar />
+                    <Container maxWidth="lg" className={classes.root}>
+                        <Switch>
+                            <Route exact path="/">
+                                <LandingPage />
+                            </Route>
+                            <Route exact path="/job/:id">
+                                <JobPageContainer />
+                            </Route>
+                        </Switch>
+                    </Container>
+                </JobContextProvider>
+            </Router>
+        </MuiThemeProvider>
     );
 }
 
